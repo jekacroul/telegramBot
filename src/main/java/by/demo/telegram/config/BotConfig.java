b00575fc5af87c77f759762bf321e41e97a4e6bf
Evgeny Norkin <116304310+jekacroul@users.noreply.github.com>
2026-04-26 19:38:22 +0300

    Skip Telegram bot registration when credentials are missing

--- src/main/java/by/demo/telegram/config/BotConfig.java
+++ src/main/java/by/demo/telegram/config/BotConfig.java
@@ -2,17 +2,24 @@ package by.demo.telegram.config;
 
 
 import by.demo.telegram.controller.BotController;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
+@Slf4j
 public class BotConfig {
     @Bean
     public TelegramBotsApi telegramBotsApi(BotController botController) throws Exception {
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(botController);
+        if (StringUtils.hasText(botController.getBotToken()) && StringUtils.hasText(botController.getBotUsername())) {
+            botsApi.registerBot(botController);
+        } else {
+            log.warn("Telegram bot registration skipped: set BOT_TOKEN (or botToken) and spring.application.name");
+        }
         return botsApi;
     }
 }
